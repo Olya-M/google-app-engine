e250aa4f1f695469147d7da319007d11289303f8
Olya-M <68296887+Olya-M@users.noreply.github.com>
2020-07-14 15:04:42 -0400

    Update server.py
    modified to classify between violins, violas, and cellos

--- app/server.py
+++ app/server.py
@@ -7,9 +7,9 @@ from io import BytesIO
 
 from fastai.vision import *
 
-model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
+model_file_url = 'https://drive.google.com/uc?export=download&id=11z2h2YTXN9lfyaJzuLrABkPIn8dV4Irl'
 model_file_name = 'model'
-classes = ['black', 'grizzly', 'teddys']
+classes = ['violin', 'viola', 'cello']
 path = Path(__file__).parent
 
 app = Starlette()
